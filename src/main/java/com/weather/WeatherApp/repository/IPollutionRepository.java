16fa05e562aee0107d1eeda86b3fa19de1f78f01
Yagiz Basaran <bompowmsp@gmail.com>
2024-07-22 17:08:16 +0300

    chore: Added function to IPollutionRepository

--- src/main/java/com/weather/WeatherApp/repository/IPollutionRepository.java
+++ src/main/java/com/weather/WeatherApp/repository/IPollutionRepository.java
@@ -4,6 +4,12 @@ import com.weather.WeatherApp.model.Pollution;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface IPollutionRepository extends JpaRepository<Pollution, Integer> {
+
+    List<Pollution> findByCityAndDateBetween(String cityName, LocalDate startDate, LocalDate endDate);
+
 }
