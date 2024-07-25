f7fe62d823d8e943dbea0b2f5d696326e1ea9b50
Yagiz Basaran <bompowmsp@gmail.com>
2024-07-25 12:15:20 +0300

    feat: Creating a fullstack app

--- src/main/java/com/weather/WeatherApp/exceptions/InvalidDateException.java
+++
@@ -1,7 +0,0 @@
-package com.weather.WeatherApp.exceptions;
-
-public class InvalidDateException extends RuntimeException {
-    public InvalidDateException(String message) {
-        super(message);
-    }
-}
